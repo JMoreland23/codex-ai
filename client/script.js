ad79372577a8d7674aab7bc1c4d6f0e24dc176ee
JMoreland23 <38889945+JMoreland23@users.noreply.github.com>
2023-01-20 14:09:55 -0500

    update the server URL

--- client/script.js
+++ client/script.js
@@ -79,7 +79,7 @@ loader(messageDiv);
 
 //fetch data from server
 
-const response = await fetch('http://localhost:5000', {
+const response = await fetch('https://codex-ai-93c0.onrender.com', {
    method: 'POST',
    headers: {
     'Content-Type': 'application/json'
